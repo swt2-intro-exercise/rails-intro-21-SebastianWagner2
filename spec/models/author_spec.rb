ff77d0fa997b33f96a9add0c50cc7a5e9f08c4e9
SebastianWagner2 <82543715+SebastianWagner2@users.noreply.github.com>
2021-11-15 20:25:06 +0100

    Add test and solution for giving full name

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @author = Author.new()
+
+    @author.first_name = "Basti"
+    @author.last_name = "Wagner"
+    @author.homepage = "https://www.theraleighregister.com/basti-wagner-homepage.html"
+  end
+
+  it "allows to modify an author object" do
+    expect(@author.first_name).to eq("Basti")
+    expect(@author.last_name).to eq("Wagner")
+    expect(@author.homepage).to eq("https://www.theraleighregister.com/basti-wagner-homepage.html")
+  end
+  
+  it "gerenates the correct full name" do
+    expect(@author.name).to eq("Basti Wagner")
+  end
 end
