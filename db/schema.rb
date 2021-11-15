3b7bb882dbcf62719414353fa2e622a8424d8882
SebastianWagner2 <82543715+SebastianWagner2@users.noreply.github.com>
2021-11-15 19:38:05 +0100

    Add new author view

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_153956) do
+ActiveRecord::Schema.define(version: 2021_11_15_175742) do
 
   create_table "authors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "homepage"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- spec/factories/authors.rb
+++ spec/factories/authors.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :author do
-    
+    first_name { "MyString" }
   end
 end
