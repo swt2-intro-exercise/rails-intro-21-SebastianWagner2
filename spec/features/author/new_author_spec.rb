57004a2dfff32da52be096e16929ff3a36cdd0a6
SebastianWagner2 <82543715+SebastianWagner2@users.noreply.github.com>
2021-11-15 19:37:52 +0100

    Add test for New Author View

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -4,6 +4,10 @@ require 'rails_helper'
    it "should exist at 'new_author_path' and render withour error" do
      # https://guides.rubyonrails.org/routing.html#path-and-url-helpers
      visit new_author_path
+
+     expect(page).to have_field('author[first_name]')
+     expect(page).to have_field('author[last_name]')
+     expect(page).to have_field('author[homepage]')
    end
  end
  
