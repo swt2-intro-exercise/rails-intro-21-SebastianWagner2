c43ce5e068c119af0ee8a16751aa60f8a0dc1448
SebastianWagner2 <82543715+SebastianWagner2@users.noreply.github.com>
2021-11-17 16:46:56 +0100

    Notify user with error message when author could not be saved

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -9,13 +9,21 @@ class AuthorsController < ApplicationController
     end
 
     def new
+        @author = Author.new
     end
 
     def create
-        @author = Author.new(params.require(:author).permit(:first_name, :last_name, :homepage))
+        @author = Author.new(author_params)
 
-        @author.save
-        redirect_to root_path, notice:'Success!'
+        if @author.save
+            redirect_to @author
+        else
+            render 'new'
+        end
     end
 
+    private
+        def author_params
+            params.require(:author).permit(:first_name, :last_name, :homepage)
+        end
 end
