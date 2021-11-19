d52ebf168daf59fb8402e027367b6a1d0da5e7b3
SebastianWagner2 <82543715+SebastianWagner2@users.noreply.github.com>
2021-11-19 19:48:26 +0100

    Add validations for paper and add error message for users

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,2 +1,5 @@
 class Paper < ApplicationRecord
+    validates :title, presence: true
+    validates :venue, presence: true
+    validates :year, presence: true, format: { with: /[0-9]*/, message: "only allows numbers" }
 end
