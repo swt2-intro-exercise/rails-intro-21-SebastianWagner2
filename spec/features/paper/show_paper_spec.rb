4c705236387627616735fdc04d69a268d325be63
SebastianWagner2 <82543715+SebastianWagner2@users.noreply.github.com>
2021-11-20 14:43:34 +0100

    Edit file name of show paper tests to the right one and set up tests for m:n relation between paper and author

--- spec/features/paper/show_paper_spec.rb
+++ spec/features/paper/show_paper_spec.rb
@@ -12,5 +12,15 @@ describe "Show paper page", type: :feature do
     expect(page).to have_text("1950")
   end
 
+  it "should show the author's full name" do
+    @paper = FactoryBot.create :paper
+
+    @paper.authors = build_list :author, 1
+
+    visit paper_path(@paper)
+    
+    expect(page).to have_text("Alan Turing")
+  end
+
 end
  
